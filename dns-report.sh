505126f880c46ddaf5c0a44c91737cd52083fe47
Uwe Sommer <uwe@usommer.de>
2017-12-26 15:24:36 +0100

    optimizations

--- dns-report.sh
+++ dns-report.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ## This script creates a table for SOA and MX Records for a given domainlist
 
-domainlist=/path/to/domainlist
+domainlist=domainlist.txt
 
-( printf "DOMAIN SOA EMAIL SERIAL REFRESH RETRY EXPIRE TTL PRIO MX PRIO MX\n" ; cat $domainlist | xargs -n1 -I_domain -- sh -c 'echo "_domain" $(dig +short soa _domain) $(dig +short mx _domain)' )| column -t
+( printf "DOMAIN SOA EMAIL SERIAL REFRESH RETRY EXPIRE TTL PRIO MX PRIO MX\n" ; xargs -n1 -I_domain -- sh -c 'echo "_domain" $(dig +short soa _domain) $(dig +short mx _domain)' < "$domainlist") | column -t
 
