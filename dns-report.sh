843c82d9e861fcf1230e0648f2af82ad2a6486c2
Uwe Sommer <uwe@usommer.de>
2017-12-26 11:16:47 +0100

    Update dns-report.sh

--- dns-report.sh
+++ dns-report.sh
@@ -3,4 +3,4 @@
 
 domainlist=path/to/domainlist
 ( printf "DOMAIN SOA EMAIL SERIAL REFRESH RETRY EXPIRE TTL PRIO MX PRIO MX\n" ; cat $domainlist | xargs -n1 -I_domain -- sh -c 'echo "_domain" $(dig +short soa _domain) $(dig +short mx _domain)' )| column -t
-@usommer
+
